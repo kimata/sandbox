15cf54ea0cf0e3c1906796859a6a6901b179bde4
KIMATA Tetsuya <kimata@green-rabbit.net>
2014-05-11 23:59:56 +0900

    add test.rb

--- ruby/test.rb
+++ ruby/test.rb
@@ -4,18 +4,50 @@
 $LOAD_PATH.push('./lib')
 
 require 'wordutil'
+require 'excelutil'
 require 'findutil'
 
-wordutil = WordUtil.new
+
 
 path = 'D:\\Document\\ssr_info\\sample'
+excel_path = 'D:\\Document\\ssr_info\\sample\\info.xls'
 
-FindUtil.find(path, {
-                file_filter: lambda {|name| /^[^~].*\.doc$/.match(name) },
-                dir_filter: lambda {|name| !/old/.match(name) },
-              }) {|path, rel_path|
+page_info = {}
 
-  doc = wordutil.open(path)
-  puts "name: %-20s, page:%d\n" % [Pathname.new(path).basename, doc.get_total_page_number]
-}
+begin
+  wordutil = WordUtil.new
+  FindUtil.find(path, {
+                  file_filter: lambda {|name| /^[^~].*\.doc$/.match(name) },
+                  dir_filter: lambda {|name| !/old/.match(name) },
+                }) {|path, rel_path|
+    doc = wordutil.open(path)
+    page_info[Pathname.new(path).basename.to_s] = doc.get_total_page_number
+    doc.close
+  }
+ensure
+  wordutil.quit  
+end
+
+
+
+excelutil = ExcelUtil.new
+
+begin
+  workbook = excelutil.open
+  sheet = workbook.get_sheet(1)
+
+  sheet.rows[1].columns[1] = 'ファイル'
+  sheet.rows[1].columns[2] = 'ページ数'
+
+
+  page_info.keys.each_with_index{|key, i| 
+    sheet.rows[2+i].columns[1] = key
+    sheet.rows[2+i].columns[2] = page_info[key]
+  }
+  workbook.save_as(excel_path)
+ensure
+  excelutil.quit  
+end
+
+
 
